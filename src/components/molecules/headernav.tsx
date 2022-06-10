e3831cb4d2b5ac29d1118cdaa8a5f85207915c40
sol <ocosio0907@gmail.com>
2022-06-10 18:07:41 +0900

    add header navs

---
+++ src/components/molecules/headernav.tsx
@@ -0,0 +1,11 @@
+export const HeaderNav = () => {
+  return (
+    <div className='flex justify-end items-center gap-6 p-2 bg-base-200 text-sm'>
+      <p>ヘルプ</p>
+      <p>|</p>
+      <p>ここから始めよう</p>
+      <p>|</p>
+      <p>ログイン</p>
+    </div>
+  )
+}

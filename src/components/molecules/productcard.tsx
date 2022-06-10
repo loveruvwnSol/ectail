be492b6e52c62abfb318de02c6ee3942ee0ebba6
sol <ocosio0907@gmail.com>
2022-06-10 18:50:00 +0900

    add nike logo

--- src/components/molecules/productcard.tsx
+++ src/components/molecules/productcard.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineHeart } from "@react-icons/all-files/ai/AiOutlineHeart"
 export const ProductCard = () => {
   return (
     <div className='rounded-none card w-80 bg-base-100 shadow-xl'>
@@ -5,10 +6,15 @@ export const ProductCard = () => {
         <img src='https://api.lorem.space/image/shoes?w=400&h=225' alt='Shoes' />
       </figure>
       <div className='card-body'>
-        <h2 className='card-title'>
-          Shoes!
-          <div className='badge badge-secondary'>NEW</div>
-        </h2>
+        <div className='flex gap-20'>
+          <h2 className='card-title'>
+            Shoes!
+            <div className='badge badge-secondary'>NEW</div>
+          </h2>
+          <div className='h-9 w-9 border-2 border-slate-800 rounded-2xl flex items-center justify-center'>
+            <AiOutlineHeart className='h-7 w-7' />
+          </div>
+        </div>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className='card-actions justify-end'>
           <div className='badge badge-outline'>Fashion</div>
